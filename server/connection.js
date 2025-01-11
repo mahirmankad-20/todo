fb63ee42d995b40ba21c18ae20bc2dbe5e655b07
mahir <mahir.mankad.ordex@gmail.com>
2025-01-12 00:01:00 +0530

    create a project insialization set up

---
+++ server/connection.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql');
+
+const con = mySql.createConnection({
+    host: "localhost",
+    user: "root", // Corrected property name
+    password: "Hulktnt5",
+    database: "todos"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    
+});
+
+module.exports = con;
